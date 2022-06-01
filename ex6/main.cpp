a37d78cae7959ca2011784d4500bc81d15471889
neta-r <neta2611@walla.com>
2022-06-01 20:31:53 +0300

    added cond after enQ

--- ex6/main.cpp
+++ ex6/main.cpp
@@ -19,8 +19,7 @@
 void *queue1;
 void *queue2;
 void *queue3;
-
-char response[MAX_SIZE];
+pipeline *pipeline1;
 
 // queue functions
 node *newNode(void *k) {
@@ -58,6 +57,7 @@ void enQ(void *q, void *n) {
     node *temp = newNode(n);
     if (the_queue->rear == nullptr) { //empty queue
         the_queue->front = the_queue->rear = temp;
+        pthread_cond_signal(&the_queue->cond);
         pthread_mutex_unlock(&the_queue->lock);
         return;
     }
@@ -103,6 +103,7 @@ void *run(void *arg) {
 
 void destroyAO(ao *active_obj) {
     pthread_cancel(active_obj->thread);
+    free(active_obj);
 }
 
 ao newAO(void *q, beforeFun beforePtr, afterFun afterPtr) {
@@ -146,13 +147,15 @@ char cesare_cipher_char(char c) {
 }
 
 void *caesar_cipher(void *mes) {
-    message *m = (message *) mes;
-    char *str = (char *) m->data;
+//    message *m = (message *) mes;
+//    char *str = (char *) m->data;
+    char *str = (char *) mes;
     for (size_t i = 0; i < strlen(str); i++) {
         str[i] = cesare_cipher_char(str[i]);
     }
-    m->data = str;
-    return (void *) m;
+//    m->data = str;
+//    return (void *) m;
+    return (void *) str;
 }
 
 char small_big_letters_char(char c) {
@@ -167,13 +170,15 @@ char small_big_letters_char(char c) {
 }
 
 void *small_big_letters(void *mes) {
-    message *m = (message *) mes;
-    char *str = (char *) m->data;
+//    message *m = (message *) mes;
+//    char *str = (char *) m->data;
+    char *str = (char *) mes;
     for (size_t i = 0; i < strlen(str); i++) {
         str[i] = small_big_letters_char(str[i]);
     }
-    m->data = str;
-    return (void *) m;
+//    m->data = str;
+//    return (void *) m;
+    return (void *) str;
 }
 
 void destroy_pipeline(pipeline *p) {
@@ -199,11 +204,20 @@ message *create_massage(int sock, char *data) {
     return mes;
 }
 
+void *stam(void *) {}
+
+void *print_response(void *args) {
+    char *m = (char *) args;
+    printf("%s", m);
+}
+
 pipeline *create_pipeline() {
     pipeline *p = (pipeline *) malloc(sizeof(pipeline));
     p->ao1 = newAO(queue1, (beforeFun) caesar_cipher, (afterFun) enQ_to_queue2);
     p->ao2 = newAO(queue2, (beforeFun) small_big_letters, (afterFun) enQ_to_queue3);
-    p->ao3 = newAO(queue3, (beforeFun) send_response, (afterFun) destroy_message);
+    p->ao2 = newAO(queue2, (beforeFun) stam, (afterFun) print_response);
+
+//    p->ao3 = newAO(queue3, (beforeFun) send_response, (afterFun) destroy_message);
     return p;
 }
 
@@ -225,12 +239,13 @@ void *socketThread(void *arg) {
                 message *mes = create_massage(newSocket, data);
                 enQ(queue1, mes);
                 sleep(1);
-                pipe = create_pipeline();
-//                destroy_pipeline(pipe);
+                if (pipe == nullptr) {
+                    pipe = create_pipeline();
+                }
             }
         } else if (strcmp(buf, "EXIT") == 0) {
             printf("client %d disconnected\n", self);
-//            destroy_pipeline(pipe);
+            destroy_pipeline(pipe);
             close(newSocket);
             pthread_exit(NULL);
         }
@@ -251,91 +266,99 @@ int main(void) {
     queue1 = createQ();
     queue2 = createQ();
     queue3 = createQ();
-    int sockfd, new_fd;  // listen on sock_fd, new connection on new_fd
-    struct addrinfo hints, *servinfo, *p;
-    struct sockaddr_storage their_addr; // connector's address information
-    socklen_t sin_size;
-    struct sigaction sa;
-    int yes = 1;
-    char s[INET6_ADDRSTRLEN];
-    int rv;
-
-    memset(&hints, 0, sizeof hints);
-    hints.ai_family = AF_UNSPEC;
-    hints.ai_socktype = SOCK_STREAM;
-    hints.ai_flags = AI_PASSIVE; // use my IP
-
-    if ((rv = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0) {
-        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
-        return 1;
+    pipeline1 = create_pipeline();
+    while (1) {
+        char data[1024];
+        memset(data, 0, 1024);
+        printf("please enter the string you want : ");
+        scanf("%s", data);
+        enQ(queue1, data);
     }
-
-    // loop through all the results and bind to the first we can
-    for (p = servinfo; p != NULL; p = p->ai_next) {
-        if ((sockfd = socket(p->ai_family, p->ai_socktype,
-                             p->ai_protocol)) == -1) {
-            perror("server: socket");
-            continue;
-        }
-
-        if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes,
-                       sizeof(int)) == -1) {
-            perror("setsockopt");
-            exit(1);
-        }
-
-        if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
-            close(sockfd);
-            perror("server: bind");
-            continue;
-        }
-
-        break;
-    }
-
-    freeaddrinfo(servinfo); // all done with this structure
-
-    if (p == NULL) {
-        fprintf(stderr, "server: failed to bind\n");
-        exit(1);
-    }
-
-    if (listen(sockfd, BACKLOG) == -1) {
-        perror("listen");
-        exit(1);
-    }
-
-    sigemptyset(&sa.sa_mask);
-    sa.sa_flags = SA_RESTART;
-    if (sigaction(SIGCHLD, &sa, NULL) == -1) {
-        perror("sigaction");
-        exit(1);
-    }
-
-    printf("server: waiting for connections...\n");
-    pthread_t tid[60];
-    int i = 0;
-    while (1) {  // main accept() loop
-        sin_size = sizeof their_addr;
-        new_fd = accept(sockfd, (struct sockaddr *) &their_addr, &sin_size);
-        if (new_fd == -1) {
-            perror("accept");
-            continue;
-        }
-
-        inet_ntop(their_addr.ss_family,
-                  get_in_addr((struct sockaddr *) &their_addr),
-                  s, sizeof s);
-        printf("server: got connection from %s\n", s);
-
-        //for each client request creates a thread and assign the client request to it to process
-        //so the main thread can entertain next request
-        if (pthread_create(&tid[i++], NULL, socketThread, &new_fd) != 0)
-            printf("Failed to create thread\n");
-    }
-    destoryQ(queue1);
-    destoryQ(queue2);
-    destoryQ(queue3);
-
-    return 0;
+//    int sockfd, new_fd;  // listen on sock_fd, new connection on new_fd
+//    struct addrinfo hints, *servinfo, *p;
+//    struct sockaddr_storage their_addr; // connector's address information
+//    socklen_t sin_size;
+//    struct sigaction sa;
+//    int yes = 1;
+//    char s[INET6_ADDRSTRLEN];
+//    int rv;
+//
+//    memset(&hints, 0, sizeof hints);
+//    hints.ai_family = AF_UNSPEC;
+//    hints.ai_socktype = SOCK_STREAM;
+//    hints.ai_flags = AI_PASSIVE; // use my IP
+//
+//    if ((rv = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0) {
+//        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+//        return 1;
+//    }
+//
+//    // loop through all the results and bind to the first we can
+//    for (p = servinfo; p != NULL; p = p->ai_next) {
+//        if ((sockfd = socket(p->ai_family, p->ai_socktype,
+//                             p->ai_protocol)) == -1) {
+//            perror("server: socket");
+//            continue;
+//        }
+//
+//        if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes,
+//                       sizeof(int)) == -1) {
+//            perror("setsockopt");
+//            exit(1);
+//        }
+//
+//        if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+//            close(sockfd);
+//            perror("server: bind");
+//            continue;
+//        }
+//
+//        break;
+//    }
+//
+//    freeaddrinfo(servinfo); // all done with this structure
+//
+//    if (p == NULL) {
+//        fprintf(stderr, "server: failed to bind\n");
+//        exit(1);
+//    }
+//
+//    if (listen(sockfd, BACKLOG) == -1) {
+//        perror("listen");
+//        exit(1);
+//    }
+//
+//    sigemptyset(&sa.sa_mask);
+//    sa.sa_flags = SA_RESTART;
+//    if (sigaction(SIGCHLD, &sa, NULL) == -1) {
+//        perror("sigaction");
+//        exit(1);
+//    }
+//
+//    printf("server: waiting for connections...\n");
+//    pthread_t tid[60];
+//    int i = 0;
+//    while (1) {  // main accept() loop
+//        sin_size = sizeof their_addr;
+//        new_fd = accept(sockfd, (struct sockaddr *) &their_addr, &sin_size);
+//        if (new_fd == -1) {
+//            perror("accept");
+//            continue;
+//        }
+//
+//        inet_ntop(their_addr.ss_family,
+//                  get_in_addr((struct sockaddr *) &their_addr),
+//                  s, sizeof s);
+//        printf("server: got connection from %s\n", s);
+//
+//        //for each client request creates a thread and assign the client request to it to process
+//        //so the main thread can entertain next request
+//        if (pthread_create(&tid[i++], NULL, socketThread, &new_fd) != 0)
+//            printf("Failed to create thread\n");
+//    }
+//    destoryQ(queue1);
+//    destoryQ(queue2);
+//    destoryQ(queue3);
+//
+//    return 0;
 }

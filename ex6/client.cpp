662f4c4c5989d54681afa00330105f10fbc1b781
reut <reuthadad123@gmail.com>
2022-06-04 17:24:08 +0300

    changed to localhost IP address

--- ex6/client.cpp
+++ ex6/client.cpp
@@ -29,7 +29,7 @@ int main() {
     hints.ai_family = AF_UNSPEC;
     hints.ai_socktype = SOCK_STREAM;
 
-    if ((rv = getaddrinfo("neta", PORT, &hints, &servinfo)) != 0) {
+    if ((rv = getaddrinfo("127.0.0.1", PORT, &hints, &servinfo)) != 0) {
         fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
         return 1;
     }

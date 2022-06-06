31e1d12d55934fe978699fa5ef30dae032784005
neta-r <neta2611@walla.com>
2022-06-06 19:25:17 +0300

    started ex7

---
+++ ex7/myfs.h
@@ -0,0 +1,27 @@
+#include <aio.h>
+
+# define FILES_MAX 10000
+typedef struct myDIR{
+
+}myDIR;
+
+typedef struct mydirent{
+    char* name_d;
+}mydirent;
+
+typedef struct myopenfile{
+
+}myopenfile;
+
+void mymkfs();
+int mymount(const char *source, const char *target,
+            const char *filesystemtype, unsigned long
+            mountflags, const void *data);
+int myopen(const char *pathname, int flags);
+int myclose(int myfd);
+ssize_t myread(int myfd, void *buf, size_t count);
+ssize_t mywrite(int myfd, const void *buf, size_t count);
+off_t mylseek(int myfd, off_t offset, int whence);
+myDIR *myopendir(const char *name);
+struct mydirent *myreaddir(myDIR *dirp);
+int myclosedir(myDIR *dirp);

20cd93deb6e6965636d1577cecd7ccb365e2c2f5
neta-r <neta2611@walla.com>
2022-06-10 18:04:21 +0300

    open and close

--- ex7/myfs.c
+++ ex7/myfs.c
@@ -2,61 +2,76 @@
 # include "myfs.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <fcntl.h>
 
 #define BLOCKSIZE 512
 
 void mymkfs(int fs_size) {
 
-    int size = fs_size - (int)sizeof (ginfo);
-
-    ginfo.num_inodes = (int)((size*0.1)/ sizeof(struct inode));
-    ginfo.num_blocks = (int)((size*0.9) / sizeof(struct disk_info));
-    ginfo.size_blocks = (int)((size*0.9));
-
-    int i;
-    inodes = malloc (sizeof (struct inode) * ginfo.num_inodes);
-    for(i = 0; i<ginfo.num_inodes; i++) {
+    int size = fs_size - (int)sizeof(sb);
+    sb.num_inodes = (int)((size*0.1)/ sizeof(inode));
+    sb.num_blocks = (int)((size*0.9) / sizeof(disk_block));
+    sb.size_blocks = (int)((size*0.9));
+    inodes = malloc (sizeof (struct inode) * sb.num_inodes);
+    for(int i = 0; i<sb.num_inodes; i++) {
         inodes[i].size = -1;
         inodes[i].first_block = -1;
         strcpy (inodes[i].name, "");
     }
 
-    dinfo = malloc (sizeof (struct disk_info) * ginfo.size_blocks);
-    for (i=0; i<ginfo.num_blocks; i++) {
-        dinfo[i].next_block_num = -1;
+    d_block = malloc (sizeof (disk_block) * sb.size_blocks);
+    for (int i=0; i<sb.num_blocks; i++) {
+        d_block[i].next_block_num = -1;
+    }
+
+    for (int i=0; i<FILES_MAX; i++){
+        myopenfile* openfile =malloc (sizeof (myopenfile));
+        openfile->fd=-1;
+        open_f[i] = *openfile;
     }
 }
 
 int mymount(const char *source, const char *target,
             const char *filesystemtype, unsigned long
             mountflags, const void *data){
-    if (source == NULL){
-        printf("No device attached.");
+    if (source == NULL || target == NULL){
+        perror("No source or target");
         return -1;
     }
     FILE *file = fopen(source, "r");
     // super block
-    fread(&ginfo, sizeof(struct general_info), 1, file);
+    fread(&sb, sizeof(superblock), 1, file);
 
     // inodes
-    inodes = malloc(sizeof(struct inode) * ginfo.num_inodes);
-    fread(inodes, sizeof(struct inode), ginfo.num_inodes, file);
+    inodes = malloc(sizeof(inode) * sb.num_inodes);
+    fread(inodes, sizeof(struct inode), sb.num_inodes, file);
 
     // dbs
-    dinfo = malloc(sizeof(struct general_info) * ginfo.num_blocks);
-    fread(dinfo, sizeof(struct disk_info), ginfo.num_blocks, file);
+    d_block = malloc(sizeof(superblock) * sb.num_blocks);
+    fread(d_block, sizeof(disk_block), sb.num_blocks, file);
     fclose(file);
     return 0;
 }
 
 void shorten_file(int bn)
 {
-    int nn = dinfo[bn].next_block_num;
-    if(dinfo[bn].next_block_num >= 0)
+    int nn = d_block[bn].next_block_num;
+    if(d_block[bn].next_block_num >= 0)
     {
         shorten_file(nn);
     }
-    dinfo[bn].next_block_num = -1;
+    d_block[bn].next_block_num = -1;
+}
+int find_empty_block()
+{
+    for (int i = 0; i < sb.num_blocks; i++)
+    {
+        if (d_block[i].next_block_num == -1)
+        {
+            return i;
+        }
+    }
+    return -1;
 }
 
 void set_filesize(int filenum, int size)
@@ -68,18 +83,18 @@ void set_filesize(int filenum, int size)
     for (num-- ; num > 0; num--)
     {
         //check next block number
-        int next_num = dinfo[bn].next_block_num;
+        int next_num = d_block[bn].next_block_num;
         if (next_num == -2)
         {
             int empty = find_empty_block();
-            dinfo[bn].next_block_num = empty;
-            dinfo[empty].next_block_num = -2;
+            d_block[bn].next_block_num = empty;
+            d_block[empty].next_block_num = -2;
         }
-        bn = dinfo[bn].next_block_num;
+        bn = d_block[bn].next_block_num;
     }
     //short the file if necessary
     shorten_file(bn);
-    dinfo[bn].next_block_num = -2;
+    d_block[bn].next_block_num = -2;
 }
 
 int get_block_num(int file, int offeset)
@@ -87,19 +102,48 @@ int get_block_num(int file, int offeset)
     int bn = inodes[file].first_block;
     for(int togo = offeset; togo > 0; togo--)
     {
-        bn = dinfo[bn].next_block_num;
+        bn = d_block[bn].next_block_num;
     }
     return bn;
 }
 
-int find_empty_block()
-{
-    for (int i = 0; i < ginfo.num_blocks; i++)
-    {
-        if (dinfo[i].next_block_num == -1)
-        {
+int find_empty_openfile(){
+    for (int i=0; i<FILES_MAX; i++){
+        if(open_f[i].fd==-1){
             return i;
         }
     }
     return -1;
+}
+
+int myopen(const char *pathname, int flags){
+    int index = find_empty_openfile();
+    if (index==-1){
+        perror("Too many files are open");
+        return -1;
+    }
+    int file = open(pathname, flags, "r");
+    open_f[index].fd = file;
+    open_f[index].pos = 0;
+    return file;
+}
+
+int open_index(int myfd){
+    for (int i=0; i<FILENAME_MAX; i++){
+        if (open_f[i].fd==myfd){
+            return i;
+        }
+    }
+    return -1;
+}
+
+int myclose(int myfd){
+    int index = open_index(myfd);
+    if (index == -1){
+        perror("You are trying to close a file that is not open!");
+        return -1;
+    }
+    open_f[index].fd=-1;
+    open_f[index].pos=0;
+    return 0;
 }
--- ex7/myfs.h
+++ ex7/myfs.h
@@ -1,37 +1,53 @@
-#include <aio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-# define FILES_MAX 10000
+#define PATH_MAX 128
+#define BLOCKSIZE 512
+#define NAME_SIZE 9
+#define MAX_DIR 10
+#define BUFF_SIZE 80
+#define FILES_MAX 10000
 
-struct general_info {
+typedef struct myopenfile{
+    int fd;
+    int pos;
+}myopenfile;
+
+typedef struct superblock{
     int num_inodes;
     int num_blocks;
     int size_blocks;
-}ginfo;
+}superblock;
 
-struct inode {
+typedef struct inode{
     int size;
     int first_block;
-    char name[8];
-}*inodes;
+    char name[NAME_SIZE+1];
+    int dir; //0 file 1 dir
+}inode;
 
-struct disk_info {
+typedef struct disk_block{
     int next_block_num;
-    char data[512];
-}*dinfo;
+    char data[BLOCKSIZE];
+}disk_block;
 
-typedef struct myDIR{
-
-}myDIR;
-
-typedef struct mydirent{
-    char* name_d;
+typedef struct mydirent {
+    int size;
+    int fds[MAX_DIR];
+    char d_name[NAME_SIZE];
 }mydirent;
 
-typedef struct myopenfile{
+typedef struct myDIR {
+    int path;
+}myDIR;
 
-}myopenfile;
+myopenfile open_f [FILES_MAX];
+superblock sb;
+inode *inodes;
+disk_block *d_block;
 
-void mymkfs(int fs_size);
+// exercise's functions
 int mymount(const char *source, const char *target,
             const char *filesystemtype, unsigned long
             mountflags, const void *data);
@@ -42,9 +58,19 @@ ssize_t mywrite(int myfd, const void *buf, size_t count);
 off_t mylseek(int myfd, off_t offset, int whence);
 myDIR *myopendir(const char *name);
 struct mydirent *myreaddir(myDIR *dirp);
-int myclosedir(myDIR *dirp);
 
-int get_block_num(int file, int offeset);
+
+// other functions
+int myclosedir(myDIR *dirp);
+void mount_fs(const char *source);  // load adile system
+void sync_fs(const char *target);   // write the file system
+void print_fs(); // print out info about file system
 void set_filesize(int filenum, int size);
-void shorten_file(int bn);
-int find_empty_block();
+void write_data(int filenum, int _pos, char data);
+char read_data(int filenum, int pos);
+int create_file(int size,const char* name); // retrun file discriptor
+struct mydirent *myread_dir(int fd);
+char* read_data_test(int filenum, int pos);
+int Myopen_dir(const char *pathname);
+int open_index(int myfd);
+int find_empty_openfile();
